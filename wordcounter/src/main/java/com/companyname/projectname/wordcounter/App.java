c762ec3adb973b18dd48d9cf210680a76af067e7
Andeel Latif <andeel1974@hotmail.com>
2023-05-09 17:16:34 +0100

    Jira-123 First Commit

---
+++ wordcounter/src/main/java/com/companyname/projectname/wordcounter/App.java
@@ -0,0 +1,13 @@
+package com.companyname.projectname.wordcounter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
