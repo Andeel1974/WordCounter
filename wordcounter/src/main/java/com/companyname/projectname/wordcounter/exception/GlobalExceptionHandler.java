b91f274b56ec5545e72ece4d3f087c995afea1c6
Andeel1974 <133020768+Andeel1974@users.noreply.github.com>
2023-08-13 16:27:02 +0100

    Update GlobalExceptionHandler.java
    JiraXXX - Second iteration

--- wordcounter/src/main/java/com/companyname/projectname/wordcounter/exception/GlobalExceptionHandler.java
+++ wordcounter/src/main/java/com/companyname/projectname/wordcounter/exception/GlobalExceptionHandler.java
@@ -20,11 +20,10 @@ public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(EmptyWordListException.class)
 	public ResponseEntity<ExceptionResponse> handleInvalidInputException(Exception ex, WebRequest request) {
-		// Note, there is disagreement in the market place on the 'correct' HttpStatus for an empty request.
 		// BAD_REQUEST (400) chosen ahead of NO_CONTENT (204) because it allows for the response to have a 
-		// message body, i.e in this case the 'EMPTY_WORD_LIST_MESSAGE_CODE'.
+		// message body, i.e in this case the ExceptionResponse instance..
 		ExceptionResponse exceptionResponse = getExceptionResponseObject(ex, request, HttpStatus.BAD_REQUEST,
-				EMPTY_WORD_LIST_MESSAGE_CODE);
+				EMPTY_WORD_LIST_MESSAGE_CODE, WordCounterService.EMPTYWORDLIST_MESSAGE);
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 	
@@ -32,13 +31,13 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
 		logger.error("Exception" ,ex);
 		ExceptionResponse exceptionResponse = getExceptionResponseObject(ex, request, HttpStatus.INTERNAL_SERVER_ERROR,
-				INTERNAL_SERVER_ERROR_MESSAGE_CODE);
+				INTERNAL_SERVER_ERROR_MESSAGE_CODE, ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	private ExceptionResponse getExceptionResponseObject(Exception ex, WebRequest request, HttpStatus httpStatus,
-			   String messageCode) {
-		ExceptionResponse exceptionResponse = ExceptionResponse.builder().timestamp(new Date()).message(ex.getMessage())
+			   String messageCode, String message) {
+		ExceptionResponse exceptionResponse = ExceptionResponse.builder().timestamp(new Date()).message(message)
 		.messageCode(messageCode).uri(request.getDescription(false)).httpStatus(httpStatus)
 		.httpStatusCode(httpStatus.value()).build();
 		return exceptionResponse;
