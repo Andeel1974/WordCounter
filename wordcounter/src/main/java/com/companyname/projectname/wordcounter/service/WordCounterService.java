9efd2a4b4979ec3fb9a21983e535c2cb4427e1b7
Andeel1974 <133020768+Andeel1974@users.noreply.github.com>
2023-08-13 16:30:36 +0100

    Update WordCounterService.java
    JiraXXX - Second iteration

--- wordcounter/src/main/java/com/companyname/projectname/wordcounter/service/WordCounterService.java
+++ wordcounter/src/main/java/com/companyname/projectname/wordcounter/service/WordCounterService.java
@@ -9,23 +9,28 @@ import com.companyname.projectname.wordcounter.exception.EmptyWordListException;
  */
 public interface WordCounterService {
 	
+	String EMPTYWORDLIST_MESSAGE = "The list of words provided for this request is empty";
+	
 	/**
 	 * Adds one or more words to the word counter.
 	 * @param words {@link java.util.List} of words to add to the word counter.
-	 * @return List<String> where each entry is a word that has been added to the library of words
+	 * @return {@link java.util.List} of {@link java.lang.String} where each entry 
+	 * is a word that has been added to the library of words.
 	 * @throws EmptyWordListException
 	 */
 	List<String> addWords(List<String> words) throws EmptyWordListException;
 	
 	/**
-	 * Returns the count of how many times a given word was added to the word counter.
+	 * Returns the count of how many times the <code>word</code> argument 
+	 * was added to the library of words.
 	 * @param word Given word for which the count is required.
-	 * @return returns the count of how many times a given word was added to the word counter.
+	 * @return returns the count of how many times the <code>word</code> argument 
+	 * was added to the library of words.
 	 */
 	long countWord(String word);
 	
 	/**
-	 * Clears the internal library of words.
+	 * Clears the library of words.
 	 */
 	void clearWordStore();
 
