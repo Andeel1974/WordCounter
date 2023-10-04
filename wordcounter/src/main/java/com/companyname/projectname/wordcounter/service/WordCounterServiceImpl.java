39d038a9c55a94e54b6b58c5d56678ee8915ba42
Andeel1974 <133020768+Andeel1974@users.noreply.github.com>
2023-10-04 16:51:19 +0100

    Update WordCounterServiceImpl.java

--- wordcounter/src/main/java/com/companyname/projectname/wordcounter/service/WordCounterServiceImpl.java
+++ wordcounter/src/main/java/com/companyname/projectname/wordcounter/service/WordCounterServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
